492865eccb95586470ab2bea521d74f011e521f7
Nitesh Chainani <niteshchainani@yahoo.com>
2018-09-24 03:19:20 +0530

    Final Commit

--- src/main/scala/GeneralLedgerProcessing.scala
+++ src/main/scala/GeneralLedgerProcessing.scala
@@ -1,6 +1,5 @@
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.DataFrame
 import scala.util.control.NonFatal
 import org.apache.spark.sql.functions.{col, _}
 
@@ -14,56 +13,24 @@ try{
     val spark=SparkSession.builder().appName("GeneralLedgerProcessing").config(conf).getOrCreate()
     spark.sparkContext.setLogLevel("ERROR")
 
-    val inputTransactions=spark.read.option("multiLine","True").json("./UBS/1537277231233_Input_Transactions.json").toDF()
+    val inputTransactions=spark.read.option("multiLine","True").json("./UBS/1537277231233_Input_Transactions.json")
     val inputTransactionsByType = inputTransactions.groupBy(col("Instrument"), col("TransactionType")).agg(sum("TransactionQuantity") as "totalQuantity")
+    val externalIinputTransactionsByType = inputTransactionsByType.withColumn("tempQuantity", when(col("TransactionType") === "B", col("totalQuantity")).otherwise(col("totalQuantity") * -1)).withColumn("AccountType1", lit("E"))
+    val internalIinputTransactionsByType = inputTransactionsByType.withColumn("tempQuantity", when(col("TransactionType") === "B", col("totalQuantity") * -1).otherwise(col("totalQuantity") )).withColumn("AccountType1", lit("I"))
+    val finalTransactionType = externalIinputTransactionsByType.union(internalIinputTransactionsByType).groupBy(col("Instrument"), col("AccountType1")).agg(sum("tempQuantity") as "derivedquantity")
 
-  /*  val inputTransactionsRDD = inputTransactions.rdd.map(x=>(x(0).toString,(x(1).asInstanceOf[Long],x(2).asInstanceOf[Long],x(3).toString))).groupByKey()
-    val dayPositions=spark.sparkContext.textFile("./UBS/Input_StartOfDay_Positions.txt")
-    val dayPositionsMappedRDD=dayPositions.mapPartitionsWithIndex((index,itr)=> if(index==0) itr.drop(1) else itr)
-      .map(
-        rec => {var temp=rec.split(",")
-          (temp(0),(temp(1).toInt,temp(2),temp(3).toLong))
-               }
-          )
-    val joinedResult =dayPositionsMappedRDD.join(inputTransactionsRDD)
-
-    joinedResult.map(rec=>(rec._1,rec._2._1._1,rec._2._1._2,computeNetQuantity(rec._2._2,rec._2._1._3,rec._2._1._2))).collect().foreach(println)
-    */
     val dayPositions = spark.read.format("csv").option("header", "true").load("./UBS/Input_StartOfDay_Positions.txt")
 
-    val calculatedDF = dayPositions.join(inputTransactionsByType, dayPositions("Instrument") === inputTransactionsByType("Instrument"), "leftouter").drop(inputTransactionsByType("Instrument")).withColumn("derivedQuantity", when((col("AccountType") === "I" && col("TransactionType") === "S") || (col("AccountType") === "E" && col("TransactionType") === "B"), col("totalQuantity") * 1).otherwise(col("totalQuantity") * -1)).na.fill(0.0).withColumn("finalQuantity", col("derivedQuantity") + col("Quantity")).groupBy(col("Instrument"), col("Account"), col("AccountType")).agg(sum("finalQuantity")-sum("Quantity") as "Delta", sum("finalQuantity") as "Quantity")
-    //calculatedDF.show()
-    calculatedDF.coalesce(1).write.csv("./UBS/END_OF_DAY_POSITION.csv")
-    calculatedDF.agg(max("Delta"),min("Delta"))
+    val calculatedDF = dayPositions.join(finalTransactionType, dayPositions("Instrument") === finalTransactionType("Instrument") && dayPositions("AccountType") === finalTransactionType("AccountType1"), "leftouter").drop(finalTransactionType("Instrument")).drop(finalTransactionType("AccountType1")).na.fill(0.0).withColumn("finalQuantity", col("derivedquantity") + col("Quantity")).withColumn("Delta" , col("finalQuantity") - col("Quantity")).select(col("Instrument"), col("Account"), col("AccountType"), col("finalQuantity") as "Quantity", col("Delta"))
+
+    calculatedDF.collect().foreach(println)
+    println("Instruments with largest and lowest net transaction volumes for the day are as below:")
+    calculatedDF.orderBy(col("Delta").desc).take(1).foreach(println)
+    calculatedDF.orderBy(col("Delta")).take(1).foreach(println)
+    calculatedDF.coalesce(1).write.option("header", "true").csv("./UBS/END_OF_DAY_POSITION")
 }
 catch {
   case NonFatal(ex) => //Log the error ex
   case ex: InterruptedException => // handle InterruptedException
-}
-  }
-
-/*  def computeNetQuantity(iterable: Iterable[(Long,Long,String)],quantity: Long,accountType: String) : Long= {
-    var finalQuantity = quantity
-    if (accountType == "E")
-   {   iterable.foreach(
-        x => {
-         if (x._3 == "B")
-         {finalQuantity = finalQuantity + x._2}
-         else
-         {finalQuantity = finalQuantity - x._2}
-        }
-        )
-  }
-    else if (accountType == "I")
-    { iterable.foreach(
-          x=>{
-              if (x._3 == "B")
-              {finalQuantity = finalQuantity - x._2}
-              else
-              {finalQuantity = finalQuantity + x._2}
-            }
-            )
-    }
-    finalQuantity
-}*/
-}
+      }
+}}
